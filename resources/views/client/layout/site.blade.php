f294388bbe0909475a29275b2ffe67016b3c002b
giorgi <gbaxbaia@gmail.com>
2021-08-03 15:44:31 +0400

    include css depend on localization

--- resources/views/client/layout/site.blade.php
+++ resources/views/client/layout/site.blade.php
@@ -6,7 +6,11 @@
         type="text/css"
         href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"
     />
-    <link rel="stylesheet" href="/client/style.css" />
+    @if(\App\Models\Language::getLocaleById(app()->getLocale())=="ge")
+        <link rel="stylesheet" href="/client/style-geo.css"/>
+    @else
+        <link rel="stylesheet" href="/client/style.css"/>
+    @endif
     <title>Tbilisi Architect</title>
     @yield('subhead')
 @endsection
@@ -17,16 +21,16 @@
     @yield('wrapper')
 
     <script
-            type="text/javascript"
-            src="https://code.jquery.com/jquery-1.11.0.min.js"
+        type="text/javascript"
+        src="https://code.jquery.com/jquery-1.11.0.min.js"
     ></script>
     <script
-            type="text/javascript"
-            src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"
+        type="text/javascript"
+        src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"
     ></script>
     <script
-            type="text/javascript"
-            src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"
+        type="text/javascript"
+        src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"
     ></script>
     <script src="{{asset('client/general.js')}}"></script>
     <script src="{{asset('client/slide.js')}}"></script>

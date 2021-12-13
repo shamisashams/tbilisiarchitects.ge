8240789f62d8acaaebbec0fd46d32d1cbf153d35
shamisashams <shamisaashams@gmail.com>
2021-12-13 13:57:14 +0400

    project detail slider

--- public/client/slide.js
+++ public/client/slide.js
@@ -1,34 +1,49 @@
 $(".news_vertical_slider").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  draggable: true,
-  arrows: true,
-  prevArrow: "#prev_news",
-  nextArrow: "#next_news",
-  dots: true,
-  speed: 900,
-  infinite: true,
-  touchThreshold: 100,
-  variableHeights: true,
-  vertical: true,
-  responsive: [
-    {
-      breakpoint: 1050,
-      settings: {
-        vertical: false,
-      },
-    },
-  ],
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    arrows: true,
+    prevArrow: "#prev_news",
+    nextArrow: "#next_news",
+    dots: true,
+    speed: 900,
+    infinite: true,
+    touchThreshold: 100,
+    variableHeights: true,
+    vertical: true,
+    responsive: [
+        {
+            breakpoint: 1050,
+            settings: {
+                vertical: false,
+            },
+        },
+    ],
 });
 $(".project_details_slider").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  draggable: true,
-  arrows: true,
-  prevArrow: "#prev_content",
-  nextArrow: "#next_content",
-  dots: false,
-  speed: 900,
-  infinite: true,
-  touchThreshold: 100,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    draggable: true,
+    arrows: true,
+    prevArrow: "#prev_content",
+    nextArrow: "#next_content",
+    dots: false,
+    speed: 400,
+    infinite: true,
+    touchThreshold: 100,
+    variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 2,
+            },
+        },
+        {
+            breakpoint: 700,
+            settings: {
+                slidesToShow: 1,
+            },
+        },
+    ],
 });
--- resources/views/client/pages/project/details.blade.php
+++ resources/views/client/pages/project/details.blade.php
@@ -20,14 +20,43 @@
 @section('wrapper')
 
     <div class="project_details">
+        <div class="title">{{$project->language(app()->getLocale())? $project->language(app()->getLocale())->title: $project->language()->title}}</div>
+        <div class="paragraph">
+        Lorem Ipsum საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა ამწყობ დაზგაზე წიგნის საცდელი ეგზემპლარი დაბეჭდა. მისი ტექსტი არამარტო 5 საუკუნის მანძილზე შემორჩა, არამედ მან დღემდე, ელექტრონული ტიპოგრაფიის დრომდეც უცვლელად მოაღწია. განსაკუთრებული პოპულარობა მას 1960-იან წლებში გამოსულმა Letraset-ის ცნობილმა ტრაფარეტებმა მოუტანა, უფრო მოგვიანებით კი — Aldus PageMaker-ის ტიპის საგამომცემლო პროგრამებმა, რომლებშიც Lorem Ipsum-ის სხვადასხვა ვერსიები იყო ჩაშენებული.
+
+        Lorem Ipsum საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა ამწყობ დაზგაზე წიგნის საცდელი ეგზემპლარი დაბეჭდა. მისი ტექსტი არამარტო 5 საუკუნის მანძილზე შემორჩა, არამედ მან დღემდე, ელექტრონული ტიპოგრაფიის დრომდეც უცვლელად მოაღწია. განსაკუთრებული პოპულარობა მას 1960-იან წლებში გამოსულმა Letraset-ის ცნობილმა ტრაფარეტებმა მოუტანა, უფრო მოგვიანებით კი — Aldus PageMaker-ის ტიპის საგამომცემლო პროგრამებმა, რომლებშიც Lorem Ipsum-ის სხვადასხვა ვერსიები იყო ჩაშენებული.
+
+        Lorem Ipsum საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა ამწყობ დაზგაზე წიგნის საცდელი ეგზემპლარი დაბეჭდა. მისი ტექსტი არამარტო 5 საუკუნის მანძილზე შემორჩა, არამედ მან დღემდე, ელექტრონული ტიპოგრაფიის დრომდეც უცვლელად მოაღწია. განსაკუთრებული პოპულარობა მას 1960-იან წლებში გამოსულმა Letraset-ის ცნობილმა ტრაფარეტებმა მოუტანა, უფრო მოგვიანებით კი — Aldus PageMaker-ის ტიპის საგამომცემლო პროგრამებმა, რომლებშიც Lorem Ipsum-ის სხვადასხვა ვერსიები იყო ჩაშენებული.
+
+
+        </div>
         <div class="project_details_slider">
             <div class="slide">
-                <div class="title">{{$project->language(app()->getLocale())? $project->language(app()->getLocale())->title: $project->language()->title}}</div>
-                <div class="paragraph">
-                    {!!$project->language(app()->getLocale())? $project->language(app()->getLocale())->content: $project->language()->content!!}
+                <div class="img">
+                    <img src="/client/img/projects/details/1.png" alt="">
                 </div>
             </div>
-            @if($project->video_link)
+            <div class="slide">
+                <div class="img">
+                    <img src="/client/img/projects/details/2.png" alt="">
+                </div>
+            </div>
+            <div class="slide">
+                <div class="img">
+                    <img src="/client/img/projects/details/1.png" alt="">
+                </div>
+            </div>
+            <div class="slide">
+                <div class="img">
+                    <img src="/client/img/projects/details/2.png" alt="">
+                </div>
+            </div>
+            <div class="slide">
+                <div class="img">
+                    <img src="/client/img/projects/details/1.png" alt="">
+                </div>
+            </div>
+            <!-- @if($project->video_link)
                 <div class="slide">
                     <div class="img">
                         <iframe src="https://www.youtube.com/embed/{{$project->video_link}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -40,7 +69,7 @@
                         <img src="{{url($file->path . '/'.$file->title)}}" alt="">
                     </div>
                 </div>
-            @endforeach
+            @endforeach -->
         </div>
         <div class="arrows">
             <button id="prev_content">
